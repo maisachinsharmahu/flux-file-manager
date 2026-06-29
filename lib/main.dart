a7959d2433514dbb7b3cda40f80bce340a35a211
maisachinsharmahu <208063415+maisachinsharmahu@users.noreply.github.com>
2026-06-29 14:15:57 +0530

    Lock device orientation to portrait mode to prevent layouts from breaking on landscape phone orientations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'app.dart';
@@ -11,6 +12,12 @@ final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Lock orientation to portrait only (industry-standard design constraint for phone file managers)
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   // Pre-initialize SharedPreferences for synchronous state access in providers
   final sharedPreferences = await SharedPreferences.getInstance();
